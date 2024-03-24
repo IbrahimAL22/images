a09ea1947432f13dfa9f6e720ef4f924b4305142
IbrahimAL22 <101429771+IbrahimAL22@users.noreply.github.com>
2024-03-24 15:24:36 +0100

    Delete images directory

--- images/app/serializers.py
+++
@@ -1,8 +0,0 @@
-# myapp/serializers.py
-from rest_framework import serializers
-from .models import UploadedImage
-
-class UploadedImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadedImage
-        fields = ('id', 'image')
--- images/images/urls.py
+++
@@ -1,6 +0,0 @@
-# images/urls.py
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include('app.urls')),
-]
